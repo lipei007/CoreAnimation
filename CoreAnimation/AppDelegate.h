9b72aa37decc20659712fe7cc1636054c528a806
mini1 <macmini1@mini1deMac-mini.local>
2016-10-20 09:18:45 +0800

    Initial Commit

---
+++ CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by Jack on 2016/10/20.
+//  Copyright © 2016年 mini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimation
+//
+//  Created by Jack on 2016/10/20.
+//  Copyright © 2016年 mini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
